530437685c80d3f0a5a3390fd427cdf4d7b9d704
shubham gupta <36784415+shubhamorai12345@users.noreply.github.com>
2018-11-01 20:43:04 +0530

    first pattern(1)
    *****
    *****
    *****
    *****
    *****

---
+++ PATTERN1.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,n;
+clrscr();
+printf("Enter the number of array");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+	for(j=0;j<n;j++)
+	{
+	 printf("*");
+	 }
+	printf("\n");
+}
+getch();
+}
